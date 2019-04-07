2e2d57badfa92f4a326fc69ef873306c590348c8
brooky56 <aidarahmetshin56@gmail.com>
2019-04-07 06:15:49 +0300

    fixing

---
+++ Core/Classes/FileUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using System.Windows.Forms;
+
+namespace GenArt.Classes
+{
+    public static class FileWorking
+    {
+        
+        public static string imageType = "jpg files (*.jpg)|*.jpg|gif files (*.gif)|*.gif|bmp files (*.bmp)|*.bmp|jpeg files (*.jpeg)|*.jpeg|All files (*.*)|*.*";
+
+        public static string saveFileName(string filter)
+        {
+            SaveFileDialog d = new SaveFileDialog();
+            d.Filter = filter;
+            if (!d.ShowDialog().Equals(DialogResult.Cancel))
+                return d.FileName;
+            return null;
+        }
+
+        public static string openFileName(string filter)
+        {
+            OpenFileDialog d = new OpenFileDialog();
+            d.Filter = filter;
+            if (!d.ShowDialog().Equals(DialogResult.Cancel))
+                return d.FileName;
+            return null;
+        }
+    }
+}
